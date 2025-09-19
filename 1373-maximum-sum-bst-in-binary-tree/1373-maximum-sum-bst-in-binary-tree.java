aad7110d50c7d2abaef6191e1f52314a63794afd
Shikhar Rai <shikharrai398@gmail.com>
2025-09-20 00:33:01 +0530

    Time: 10 ms (7.96%), Space: 59.9 MB (42.96%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+ //----------------------------App1------------------------------------
  class Info{
     boolean isValid;
     int sum;
@@ -27,10 +28,25 @@
         this.max = max;
     }
  }
+
+// ------------------------------App2-------------------------------
+class Infoo{
+    int size;
+    int max; 
+    int min;
+
+    // constructor
+    Infoo(int size, int max, int min){
+        this.size = size;
+        this.max = max;
+        this.min = min;
+    }
+}
 class Solution {
+   
     private int Maxsum = 0;
     public int maxSumBST(TreeNode root) {
-        //
+    //      //---------------------------App1--------------------------
             helper(root);
             return Maxsum;
     }
@@ -54,10 +70,30 @@ class Solution {
     else{
         return new Info(false, 0, 0, 0);
      }
-    
+     
+    //  Infoo i = helper(root);
+    //  return i.size;
     }
 
-    // calcSum
-  
+    // // helper
+    // public Infoo helper(TreeNode root){
+    //     //
+    //     if(root == null){
+    //         return new Infoo(0,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    //     }
+    //     Infoo left = helper(root.left);
+    //     Infoo right = helper(root.right);
+
+    //     int size = left.size + right.size + 1;
+    //     int max = Math.max(root.val, Math.max(left.max, right.max));
+    //     int min = Math.min(root.val, Math.min(left.min, right.min));
+
+    //     if(left.max < root.val && root.val < right.min){
+    //         return new Infoo(size, max, min);
+    //     }else{
+    //         int sizeee = Math.max(left.size, right.size);
+    //         return new Infoo(sizeee, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    //     }
+    // }
 }
  
