a4e9cba52a62c64c4f4c6b0b515abf6ac02c498f
Shikhar Rai <shikharrai398@gmail.com>
2025-09-23 01:39:47 +0530

    Time: 16 ms (95.51%), Space: 60.3 MB (43.12%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -4,18 +4,66 @@ class Solution {
         // Arrays.sort(nums);
         // return nums[nums.length-k];
 
-        // App 2
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int num : nums){
-            pq.add(num);
+        // ---------------------------------App 2----------------------------
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // for(int num : nums){
+        //     pq.add(num);
+        // }
+        // int count = 0;
+        // int r = 0;
+        // while(count != k){
+        //      r = pq.remove();
+        //     count++;
+        // }
+      
+        // return r;
+
+        //----------------------App3---------------------------------------
+        // form a minHeap with first k elements of array
+        // heapify---minHeap
+         int n = nums.length;
+         int [] heap = new int[k];
+         for(int i=0; i<k; i++){
+            heap[i] = nums[i];
+         }
+          for (int i = k / 2 - 1; i >= 0; i--) {
+            Heapify(heap, i);
         }
-        int count = 0;
-        int r = 0;
-        while(count != k){
-             r = pq.remove();
-            count++;
+
+        for(int i=k; i<n; i++){
+            if(heap[0] < nums[i]){
+                // replace heap[0] with nums[i]
+               
+                heap[0] = nums[i];
+                
+                Heapify(heap,0);
+            }
+        }
+        // now Heap has k Largest element now get the kth largest element
+          return heap[0];
+       
+    }
+        public void Heapify(int []arr, int idx){
+            int size = arr.length;
+            int minIdx = idx;
+            int left = 2*idx + 1;
+            int right = 2*idx + 2;
+
+            if(left < size && arr[left] < arr[minIdx]){
+                minIdx = left;
+            }
+            if(right < size && arr[right] < arr[minIdx]){
+                minIdx = right;
+            }
+
+            // swap(idx, minIdx)
+            if(minIdx != idx){
+                int temp = arr[idx];
+                arr[idx] = arr[minIdx];
+                arr[minIdx] = temp;
+
+                Heapify(arr,minIdx);
+            }
         }
       
-        return r;
-    }
 }
