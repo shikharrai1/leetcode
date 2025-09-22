a1185b3efe7cce635ae8c79b6ae563d7aa30e535
Shikhar Rai <shikharrai398@gmail.com>
2025-09-23 01:16:41 +0530

    Time: 73 ms (11.64%), Space: 57.7 MB (67.74%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,34 +1,21 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         // App 1
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        // Arrays.sort(nums);
+        // return nums[nums.length-k];
 
-        // ----------------------------App 2--------------------------
-        // 1st put 1st k integers in an arrayList 
-        // ArrayList<Integer> list = new ArrayList<>();
-        // for(int i=0; i<k; i++){
-        //     list.add(nums[i]);
-        // }
-       
-        
-        // // now traverse remaining elements in nums and see if nums[i] is greater than smallest element in list
-        // for(int i=k; i<nums.length; i++){
-        //    // get the smallest element out of these k elements in list
-        //    int smallestIdx = 0; // initially assuming that smallest element is present at 0th index of list
-        //    for(int j = 1; j<k; j++){
-        //        if(list.get(j) < list.get(smallestIdx)){
-        //         smallestIdx = j;
-        //        }
-        //    }
-        //    if(nums[i] > list.get(smallestIdx)){
-        //          // assign nums[i] at  this 'smallestIdx' of list 
-        //          list.remove(smallestIdx);
-        //          list.add(smallestIdx,nums[i]);
-                 
-        //    }
-        // }
-        // Collections.sort(list);
-        // return list.get(0);
-} 
+        // App 2
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : nums){
+            pq.add(num);
+        }
+        int count = 0;
+        int r = 0;
+        while(count != k){
+             r = pq.remove();
+            count++;
+        }
+      
+        return r;
+    }
 }
