6ecae6af683db2706c8a984e4a6a1da0b1af7a44
Shikhar Rai <shikharrai398@gmail.com>
2025-10-14 22:01:33 +0530

    Time: 109 ms (96.1%), Space: 101.8 MB (56.29%) - LeetHub

--- 3709-design-exam-scores-tracker/3709-design-exam-scores-tracker.java
+++ 3709-design-exam-scores-tracker/3709-design-exam-scores-tracker.java
@@ -25,7 +25,7 @@ class ExamTracker {
                 ei = mid;
             }
         }
-        return si;
+        return ei;
     }
     public long totalScore(int startTime, int endTime) {
         int leftIdx = binarySearch(startTime);
