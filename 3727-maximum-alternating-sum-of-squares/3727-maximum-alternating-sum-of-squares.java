ee9052846d3e144906a17530e0a389775517fa26
Shikhar Rai <shikharrai398@gmail.com>
2025-10-30 08:59:01 +0530

    Time: 30 ms (64.45%), Space: 63.7 MB (11.94%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
@@ -9,12 +9,12 @@ class Solution {
         // now since arr contains elements which are non negative now we will sort them 
         Arrays.sort(arr);
     
-        // now sortedarr contains element in decreasing order
+       
         long evenSum = 0;
         long oddSum = 0;
         int n = arr.length;
         // now we will first figure out size of array if it is even then there will be equal number of even and odd indices but if it's odd there will be even index one more than odd index
-        if(n%2 == 0){
+      
             for(int k = n-1; k>=n/2; k--){
                 evenSum += arr[k]*arr[k];
             }
@@ -22,14 +22,6 @@ class Solution {
                 oddSum += arr[l]*arr[l];
             }
             return evenSum - oddSum;
-        }else{
-                 for(int k = n-1; k>=n/2; k--){
-                evenSum += arr[k]*arr[k];
-            }
-            for(int l = n/2-1; l>=0; l--){
-                oddSum += arr[l]*arr[l];
-            }
-            return evenSum - oddSum;
-        }
+        
     }
 }
