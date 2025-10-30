55c8e4ed72dbfe27883a0ed39ecab2d0c50281e9
Shikhar Rai <shikharrai398@gmail.com>
2025-10-30 08:52:16 +0530

    Time: 30 ms (64.45%), Space: 63.1 MB (19.8%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
@@ -8,31 +8,26 @@ class Solution {
         }
         // now since arr contains elements which are non negative now we will sort them 
         Arrays.sort(arr);
-        int sortedarr[] = new int[nums.length];
-        int j = sortedarr.length-1;
-
-        for(int el : arr){
-               sortedarr[j] = el;
-               j--;
-        }
+    
         // now sortedarr contains element in decreasing order
         long evenSum = 0;
         long oddSum = 0;
+        int n = arr.length;
         // now we will first figure out size of array if it is even then there will be equal number of even and odd indices but if it's odd there will be even index one more than odd index
-        if(sortedarr.length%2 == 0){
-            for(int k = 0; k<sortedarr.length/2; k++){
-                evenSum += sortedarr[k]*sortedarr[k];
+        if(n%2 == 0){
+            for(int k = n-1; k>=n/2; k--){
+                evenSum += arr[k]*arr[k];
             }
-            for(int l = sortedarr.length/2; l<sortedarr.length; l++){
-                oddSum += sortedarr[l]*sortedarr[l];
+            for(int l = n/2-1; l>=0; l--){
+                oddSum += arr[l]*arr[l];
             }
             return evenSum - oddSum;
         }else{
-                 for(int k = 0; k<sortedarr.length/2+1; k++){
-                evenSum += sortedarr[k]*sortedarr[k];
+                 for(int k = n-1; k>=n/2; k--){
+                evenSum += arr[k]*arr[k];
             }
-            for(int l = sortedarr.length/2+1; l<sortedarr.length; l++){
-                oddSum += sortedarr[l]*sortedarr[l];
+            for(int l = n/2-1; l>=0; l--){
+                oddSum += arr[l]*arr[l];
             }
             return evenSum - oddSum;
         }
