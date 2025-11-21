406f760b5f15aecf2ce8aa1b983f49a4badcf194
Shikhar Rai <shikharrai398@gmail.com>
2025-11-22 01:48:20 +0530

    Time: 49 ms (5.06%), Space: 47.7 MB (21.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,49 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 // ----------------------------App1 Brute Force-----------------------------------
-        List<Integer> list = new ArrayList<>();
-        int cntZero = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != 0){
-                list.add(nums[i]);
-            }else{
-                cntZero++;
-            }
+        // List<Integer> list = new ArrayList<>();
+        // int cntZero = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     if(nums[i] != 0){
+        //         list.add(nums[i]);
+        //     }else{
+        //         cntZero++;
+        //     }
+        // }
+        // // now assign elements of list in the original array (nums) then also put cntZero number of zeros at the end of the array
+        // for(int i = 0; i<list.size(); i++){
+        //     nums[i] = list.get(i);
+        // }
+        // for(int i = 0, j = list.size(); i<cntZero; i++,j++){
+        //     nums[j] = 0;
+        // }
+
+    //-----------------------------App2 Optimal Solution-----------------------
+    int i = 0;
+    int j = 0;
+    while(i < nums.length && j < nums.length){
+        // move j until it points to an index that has zero
+        while(nums[j] != 0 && j<nums.length-1){
+            j++;
         }
-        // now assign elements of list in the original array (nums) then also put cntZero number of zeros at the end of the array
-        for(int i = 0; i<list.size(); i++){
-            nums[i] = list.get(i);
+        // make sure that i points to a nonZero element right after j points to a zero element  
+        if(j < nums.length-1){
+            i = j+1;
+             // keep moving i until it points to a non-zero element
+        while(nums[i] == 0 && i < nums.length-1){
+            i++;
         }
-        for(int i = 0, j = list.size(); i<cntZero; i++,j++){
-            nums[j] = 0;
         }
+       
+        // now j points to an index that has zero and i points to an element that has nonZero element
+        // now swap nums[i] with nums[j]
+       if(i > j){
+         int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+       }
+
+        j++;
+    }
     }
 }
