cf77b787fa8230443afd5d8015e9933e2d576fd3
Shikhar Rai <shikharrai398@gmail.com>
2025-11-22 01:45:19 +0530

    Time: 5 ms (9.99%), Space: 47.7 MB (21.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+// ----------------------------App1 Brute Force-----------------------------------
         List<Integer> list = new ArrayList<>();
         int cntZero = 0;
         for(int i = 0; i<nums.length; i++){
