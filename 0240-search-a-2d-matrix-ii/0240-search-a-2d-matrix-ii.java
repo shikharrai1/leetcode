e9befaea6b4c6563b9197b6e96e8f2c3c62d2c96
Shikhar Rai <shikharrai398@gmail.com>
2025-11-29 12:27:43 +0530

    Time: 4 ms (71.41%), Space: 48 MB (30.99%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,8 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j] == target) return true;
+        //----------------------App1-----------------------------
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if(matrix[i][j] == target) return true;
+        //     }
+        // }
+        // return false;
+     //------------------------App2(Better Solution)-------------------------
+       for(int i = 0; i<matrix.length; i++){
+        boolean ans =  BS(matrix[i], target);
+        if(ans){
+            return true;
+        }
+       }
+       return false;
+    }
+    // BS()
+    boolean BS(int arr[], int target){
+        int si = 0;
+        int ei = arr.length-1;
+        while(si <= ei){
+            int mid = (si+ei)/2;
+            if(arr[mid] == target){
+                return true;
+            }else if(arr[mid] < target){
+                si = mid+1;
+            }else{
+                ei = mid-1;
             }
         }
         return false;
