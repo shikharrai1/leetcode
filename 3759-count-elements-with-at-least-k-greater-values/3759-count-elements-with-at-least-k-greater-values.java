ecd68d626ed0e54b604fab1a0d252a1d133f618f
Shikhar Rai <shikharrai398@gmail.com>
2025-11-30 20:58:29 +0530

    Time: 40 ms (100%), Space: 134.3 MB (0%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
@@ -1,36 +1,36 @@
 class Solution {
     public int countElements(int[] nums, int k) {
-        int[] arr = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            arr[i] = nums[i];
-        }
-        Arrays.sort(arr);
-        // Now count for each element 
-        int cnt = 0;
-        for(int i=0; i<nums.length; i++){
-            int ele = nums[i];
-            int ind = upperBound(arr,ele);
-            if(arr.length-1-ind >= k){
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    int upperBound(int arr[], int target){
-        int ans = 0;
-        int si = 0;
-        int ei = arr.length-1;
-        while(si <= ei){
-            int mid = (si+ei)/2;
-            if(arr[mid] <= target){
-                ans = mid;
-                si = mid+1;
-            }else {
-                ei = mid-1;
-            }
-        }
-        return ans;
-    }
+    //     int[] arr = new int[nums.length];
+    //     for(int i=0; i<nums.length; i++){
+    //         arr[i] = nums[i];
+    //     }
+    //     Arrays.sort(arr);
+    //     // Now count for each element 
+    //     int cnt = 0;
+    //     for(int i=0; i<nums.length; i++){
+    //         int ele = nums[i];
+    //         int ind = upperBound(arr,ele);
+    //         if(arr.length-1-ind >= k){
+    //             cnt++;
+    //         }
+    //     }
+    //     return cnt;
+    // }
+    // int upperBound(int arr[], int target){
+    //     int ans = 0;
+    //     int si = 0;
+    //     int ei = arr.length-1;
+    //     while(si <= ei){
+    //         int mid = (si+ei)/2;
+    //         if(arr[mid] <= target){
+    //             ans = mid;
+    //             si = mid+1;
+    //         }else {
+    //             ei = mid-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
 //   Arrays.sort(nums);
 //   int reversed[] = new int[nums.length];
 //   for(int i=nums.length-1, j=0; i>=0; i--,j++){
@@ -39,7 +39,7 @@ class Solution {
 //   int prefix[] = new int[nums.length];
 //   prefix[0] = 0;
 //   for(int i=1; i<reversed.length; i++){
-//           if(reversed[i] > reversed[i-1]){
+//           if(reversed[i] < reversed[i-1]){
 //             prefix[i] = prefix[i-1] + 1;
 //           }else{
 //              prefix[i] = prefix[i-1];
@@ -47,8 +47,20 @@ class Solution {
 //   }
 //     int cnt = 0;
 //   for(int i = 0; i<prefix.length; i++){
-//            if(prefix[i] > k) cnt++;
+//            if(prefix[i] >= k) cnt++;
 //   }
 //   return cnt;
 
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        if (k == 0) return n;
+        if (k >= n) return 0;
+        int threshold = nums[n-k];
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < threshold) cnt++;
+        }
+        return cnt;
+}
 }
