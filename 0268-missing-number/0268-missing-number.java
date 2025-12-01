08acd69d22462404ad2d8aa016658f3f383c824b
Shikhar Rai <shikharrai398@gmail.com>
2025-12-01 15:39:47 +0530

    Time: 0 ms (100%), Space: 47 MB (42.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
-//--------------------------App2--------------------------------
-for(int i=0; i<=nums.length; i++){
-    int flag = 0;
-    for(int j=0; j<nums.length; j++){
-        if(nums[j] == i){
-            flag = 1;
-            break;
-        }
-    }
-    if(flag == 0){
-        return i;
-    }
-}
-return -1;
+//--------------------------App1--------------------------------
+// for(int i=0; i<=nums.length; i++){
+//     int flag = 0;
+//     for(int j=0; j<nums.length; j++){
+//         if(nums[j] == i){
+//             flag = 1;
+//             break;
+//         }
+//     }
+//     if(flag == 0){
+//         return i;
+//     }
+// }
+// return -1;
 //--------------------------App2--------------------------------
         // List<Integer> list = new ArrayList<>();
         // for(int i=0; i<nums.length; i++){
@@ -26,5 +26,16 @@ return -1;
         // }
         // return -1;
 
+//--------------------------App3--------------------------------
+int present[] = new int[nums.length+1];
+for(int i=0; i<nums.length; i++){
+    present[nums[i]]++;
+}
+   for(int i=0; i<present.length; i++){
+    if(present[i] == 0){
+        return i;
+    }
+   }
+   return -1;
     }
 }
