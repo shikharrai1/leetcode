7ef2bc2db645c439a146f3c5bc238aa793abf833
Shikhar Rai <shikharrai398@gmail.com>
2025-12-01 17:22:09 +0530

    Time: 537 ms (5.3%), Space: 47 MB (17.07%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-          int xor = 0;
-        for(int num : nums){
-            xor ^= num; 
+        //----------------------------Brute Force-----------------------------
+     for(int i=0; i<nums.length; i++){
+        int currEle = nums[i];
+        int cnt = 0;
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] ==  currEle) cnt++;
         }
-        return xor;
+        if(cnt == 1) return nums[i];
+     }
+     return -1;
+     
+//----------------------------Optimal Solution-------------------------------
+        //   int xor = 0;
+        // for(int num : nums){
+        //     xor ^= num; 
+        // }
+        // return xor;
     }
 }
