c479ce6945c46196f93e61f67ef06fa2a8c0661d
Shikhar Rai <shikharrai398@gmail.com>
2025-12-02 23:58:15 +0530

    Time: 5 ms (28.69%), Space: 176.6 MB (5.5%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
     int [] positives = new int[nums.length/2];
     int [] negatives = new int[nums.length/2];
-        //add positive elemenst in positives arr and negative element in negatives arr
+        //add positive elements in positives arr and negative element in negatives arr
         for(int i=0,j=0,k = 0; i<nums.length; i++){
             if(nums[i] > 0){
                 positives[j] = nums[i];
@@ -13,15 +13,12 @@ class Solution {
             }
         }
      
-        // an array to return the answer
-        int ans[] = new int[nums.length];
-        int i =  0; // to iterate on +ve elements
-        int j = 0; // to iterate on -ve elements
-        int k = 0; // to iterate over ans array
-        for(; k<ans.length; k++,i++,j++){
-            ans[k] = positives[i];
-            k++;
-            ans[k] = negatives[j];
+      int [] ans = new int[nums.length];
+       int i = 0; // iterate over positive arr
+       int j = 0; // iterate over negative arr
+        for(int k = 0; k<ans.length/2; k++,i++,j++){
+            ans[2*k] = positives[i];
+            ans[2*k+1] = negatives[j];
         }
         return ans;
     }
