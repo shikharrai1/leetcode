01f7eac4d2b5048479d93d7dba5e311a652d7ab1
Shikhar Rai <shikharrai398@gmail.com>
2025-12-02 17:31:19 +0530

    Time: 1413 ms (5.23%), Space: 55.5 MB (25.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
         //--------------------App1(Brute Force)---------------------------
-        HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-        // Now Traverse the hm to find the element that appears more than floor of n/2 times
-        for(int key : hm.keySet()){
-            if(hm.get(key) > Math.floor(nums.length/2)) return key;
+            int ele = nums[i];
+            int cnt = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[j] == ele) cnt++;
+            }
+            if(cnt > Math.floor(nums.length/2)) return nums[i];
         }
         return -1;
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        // }
+        // // Now Traverse the hm to find the element that appears more than floor of n/2 times
+        // for(int key : hm.keySet()){
+        //     if(hm.get(key) > Math.floor(nums.length/2)) return key;
+        // }
+        // return -1;
     }
 }
