60e428ee5767700ecd11609cea1f7022275642b7
Shikhar Rai <shikharrai398@gmail.com>
2025-12-02 14:25:50 +0530

    Time: 3 ms (9.81%), Space: 43.5 MB (25.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,5 +2,23 @@ class Solution {
     public void sortColors(int[] nums) {
         //----------------------App1(Brute Force)-----------------------
         Arrays.sort(nums);
+     //----------------------App2(Better Sol)---------------
+     int cntZero = 0;
+     int cntOne = 0;
+     int cntTwo = 0;
+     for(int i=0; i<nums.length; i++){
+        if(nums[i] == 0) cntZero++;
+        else if(nums[i] == 1) cntOne++;
+        else cntTwo++;
+     }
+    for(int i=0; i<cntZero; i++){
+        nums[i] = 0;
+    }
+     for(int i=0; i<cntOne; i++){
+        nums[i+cntZero] = 1;
+    }
+   for(int i=0; i<cntTwo; i++){
+        nums[i+cntOne+cntZero] = 2;
+    }
     }
 }
