5d2ef5d0500e5c689cb6e2fd09b5a285288749b2
Shikhar Rai <shikharrai398@gmail.com>
2025-12-02 17:09:01 +0530

    Time: 3 ms (9.81%), Space: 43.7 MB (7.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,22 +3,42 @@ class Solution {
         //----------------------App1(Brute Force)-----------------------
         Arrays.sort(nums);
      //----------------------App2(Better Sol)---------------
-     int cntZero = 0;
-     int cntOne = 0;
-     int cntTwo = 0;
-     for(int i=0; i<nums.length; i++){
-        if(nums[i] == 0) cntZero++;
-        else if(nums[i] == 1) cntOne++;
-        else cntTwo++;
-     }
-    for(int i=0; i<cntZero; i++){
-        nums[i] = 0;
+//      int cntZero = 0;
+//      int cntOne = 0;
+//      int cntTwo = 0;
+//      for(int i=0; i<nums.length; i++){
+//         if(nums[i] == 0) cntZero++;
+//         else if(nums[i] == 1) cntOne++;
+//         else cntTwo++;
+//      }
+//     for(int i=0; i<cntZero; i++){
+//         nums[i] = 0;
+//     }
+//      for(int i=cntZero; i<cntZero+cntOne; i++){
+//         nums[i] = 1;
+//     }
+//    for(int i=cntOne+cntZero; i<cntOne+cntZero+cntTwo; i++){
+//         nums[i] = 2;
+//     }
+//------------------------App3(Optimal Sol)-------------------
+int low = 0;
+int mid = 0;
+int high = nums.length-1;
+while(mid <= high){
+    if(nums[mid] == 0){
+        int temp = nums[low];
+        nums[low] = nums[mid];
+        nums[mid] = temp;
+        mid++;
+        low++;
+    }else if(nums[mid] == 1){
+        mid++;
+    }else{
+        int temp = nums[high];
+        nums[high] = nums[mid];
+        nums[mid] = temp;
+        high--;
     }
-     for(int i=cntZero; i<cntZero+cntOne; i++){
-        nums[i] = 1;
-    }
-   for(int i=cntOne+cntZero; i<cntOne+cntZero+cntTwo; i++){
-        nums[i] = 2;
-    }
+}
     }
 }
