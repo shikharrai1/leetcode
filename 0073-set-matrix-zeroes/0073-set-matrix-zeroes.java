cb716638e6a68756bcf8be93a1021755b8ec0803
Shikhar Rai <shikharrai398@gmail.com>
2025-12-05 02:06:14 +0530

    Time: 1 ms (63.7%), Space: 47.4 MB (11.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -41,24 +41,63 @@ class Solution {
         //     }
         // }
             //******************************************Better Sol(2)************* */
-    boolean row[] = new boolean[matrix.length];
-    boolean col[] = new boolean[matrix[0].length];
+    // boolean row[] = new boolean[matrix.length];
+    // boolean col[] = new boolean[matrix[0].length];
 
+    // for(int i=0; i<matrix.length; i++){
+    //     for(int j=0; j<matrix[0].length; j++){
+    //         if(matrix[i][j] == 0){
+    //             row[i] = true;
+    //             col[j] = true;
+    //         }
+    //     }
+    // }
+    // // now traverse the matrix again and mark those cell 0 for which either row is true or col is true
+    // for(int i=0; i<matrix.length; i++){
+    //       for(int j=0; j<matrix[0].length; j++){
+    //         if(row[i] || col[j]){
+    //             matrix[i][j] = 0;
+    //         }
+    //       }
+    // }
+    //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Optimal Sol&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // int[] row[n] -> matrix[..][0]
+    // int[] col[m] -> matrix[0][..]
+    int col0 = 1;
     for(int i=0; i<matrix.length; i++){
         for(int j=0; j<matrix[0].length; j++){
             if(matrix[i][j] == 0){
-                row[i] = true;
-                col[j] = true;
+                // mark the i-th row
+                matrix[i][0] = 0;
+                // mark the j-th column
+               if(j != 0){
+                 matrix[0][j] = 0;
+               }else{
+                col0 = 0;
+               }
             }
         }
     }
-    // now traverse the matrix again and mark those cell 0 for which either row is true or col is true
-    for(int i=0; i<matrix.length; i++){
-          for(int j=0; j<matrix[0].length; j++){
-            if(row[i] || col[j]){
+
+    // Now iterate the matrix from (1,1) to (n-1,m-1) ignoring 0th row and 0th col for every cell (i,j) check if matrix[i][0] is 0 or matrix[0][j] is 0 if yes then mark matrix[i][j] = 0;
+    for(int i=1; i<matrix.length; i++){
+        for(int j=1; j<matrix[0].length; j++){
+            if(matrix[i][0] == 0 || matrix[0][j] == 0){
                 matrix[i][j] = 0;
             }
+        }
+    }
+    // Now 0th row
+    for(int i=matrix[0].length-1; i>=1; i--){
+          if(matrix[0][0] == 0 || matrix[0][i] == 0){
+            matrix[0][i] = 0;
           }
+    }
+    // Now 0th column
+    for(int i=0; i<matrix.length; i++){
+        if(matrix[i][0] == 0 || col0 == 0){
+            matrix[i][0] = 0;
+        }
     }
     }
     // markRow()
