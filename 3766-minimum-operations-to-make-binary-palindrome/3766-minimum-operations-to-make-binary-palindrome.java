ded45edf8bbbdce9a5de6842c90cf1fd1cb27abe
Shikhar Rai <shikharrai398@gmail.com>
2025-12-08 12:50:32 +0530

    Time: 192 ms (0%), Space: 47.2 MB (100%) - LeetHub

---
+++ 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] minOperations(int[] nums) {
+        int ans[] = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            String bin = Integer.toBinaryString(num);
+            if(isPalindrome(bin)){
+                ans[i] = 0;
+                continue;
+            }
+            // now increase num and check for palindrome
+            num = num + 1;
+            while(!isPalindrome(Integer.toBinaryString(num))){
+                num = num + 1;
+            }
+            int rightDis = num - nums[i];
+            // now decrease num and check for palindrome
+            num = nums[i] - 1;
+            while(num >= 0 && !isPalindrome(Integer.toBinaryString(num))){
+                num = num - 1;
+            }
+            int leftDis = nums[i] - num;
+            int minDis = Math.min(rightDis, leftDis);
+            ans[i] = minDis;
+        }
+        return ans;
+    }
+    // isPalindrome()
+    boolean isPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
