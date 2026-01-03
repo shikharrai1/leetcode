b7e6fe14585bbeb555e14d8b110fa0a385f76ec6
Shikhar Rai <shikharrai398@gmail.com>
2026-01-03 19:33:47 +0530

    Time: 1 ms (94.56%), Space: 43.5 MB (50.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,25 +1,42 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=1; i<=numRows; i++){
-            List<Integer> eachRow = new ArrayList<>();
-            // there will be i elements in ith row 
-            for(int el = 1; el <= i; el++){
-                // NCR(row-1, col-1)
-                int res = NCR(i-1,el-1);
-                 eachRow.add(res);
-            }
-            ans.add(eachRow);
-        }
-        return ans;
-    }
-    // NCR()
-    int NCR(int row, int col){
+    //----------------App1(Brute Force)-------------------------------
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     for(int i=1; i<=numRows; i++){
+    //         List<Integer> eachRow = new ArrayList<>();
+    //         // there will be i elements in ith row 
+    //         for(int el = 1; el <= i; el++){
+    //             // NCR(row-1, col-1)
+    //             int res = NCR(i-1,el-1);
+    //              eachRow.add(res);
+    //         }
+    //         ans.add(eachRow);
+    //     }
+    //     return ans;
+    // }
+    // // NCR()
+    // int NCR(int row, int col){
+    //     int res = 1;
+    //     for(int i=0; i<col; i++){
+    //         res = res*(row-i);
+    //         res = res/(i+1);
+    //     }
+    //     return res;
+    
+
+    //---------------------App2(Optimal Sol)------------------------
+    List<List<Integer>> ans = new ArrayList<>();
+    for(int i=1; i<=numRows; i++){
+        List<Integer> oneRow =new ArrayList<>();
         int res = 1;
-        for(int i=0; i<col; i++){
-            res = res*(row-i);
-            res = res/(i+1);
+        oneRow.add(res);
+        for(int el=1; el<i; el++){
+           res = res*(i-el);
+           res = res/el;
+           oneRow.add(res);
         }
-        return res;
+        ans.add(oneRow);
     }
+    return ans;
+}
 }
