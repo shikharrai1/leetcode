6f236e6869ebd936e8c4b803d372611290aa56df
Shikhar Rai <shikharrai398@gmail.com>
2026-01-12 11:40:16 +0530

    Time: 99 ms (100%), Space: 47.1 MB (100%) - LeetHub

---
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int centeredSubarrays(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length;i++){
+            Set<Integer> set = new HashSet<>();
+            int sum = 0;
+            for(int j=i;j<nums.length;j++){
+                //from index i to index j is a subarrray
+                set.add(nums[j]);
+                sum += nums[j];
+                if(set.contains(sum)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
