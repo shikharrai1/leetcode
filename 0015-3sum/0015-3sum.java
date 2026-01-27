ad8138f434cb19407f93a1b028f0d3ddef5b2b3e
Shikhar Rai <shikharrai398@gmail.com>
2026-01-27 23:20:41 +0530

    Time: 30 ms (85.33%), Space: 60.4 MB (9.4%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int sum = 0;
+            int j = i+1;
+            int k = nums.length-1;
+
+            while(j < k){
+                sum  = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    // sum == 0
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+
+                    ans.add(temp);
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                }
+            }
+        }
+        return ans;
+        // Set<List<Integer>> ans = new HashSet<>();
+        // int n = nums.length;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         for(int k=j+1; k<n; k++){
+        //             int sum = nums[i] + nums[j] + nums[k];
+        //             if(sum == 0){
+        //                 List<Integer> list = new ArrayList<>();
+        //                 list.add(nums[i]);
+        //                 list.add(nums[j]);
+        //                 list.add(nums[k]);
+        //                 Collections.sort(list);
+        //                 ans.add(list);
+        //             }
+        //         }
+        //     }
+        // }
+        // return new ArrayList<>(ans);
+    }
+}
