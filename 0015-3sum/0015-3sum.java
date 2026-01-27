44f0b2b92560ecefefd07971af82772cdd22069e
Shikhar Rai <shikharrai398@gmail.com>
2026-01-27 23:30:32 +0530

    Time: 31 ms (79.67%), Space: 60.2 MB (16.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -25,12 +25,12 @@ class Solution {
                     j++;
                     k--;
 
-                    // while(j < k && nums[j] == nums[j-1]){
-                    //     j++;
-                    // }
-                    // while(j < k && nums[k] == nums[k+1]){
-                    //     k--;
-                    // }
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 }
             }
         }
