f2e09761796b2081b3f9f7e21afde66be87c7f03
Shikhar Rai <shikharrai398@gmail.com>
2026-02-02 13:20:53 +0530

    Time: 23 ms (44.04%), Space: 271.5 MB (34.77%) - LeetHub

--- 3819-rotate-non-negative-elements/3819-rotate-non-negative-elements.java
+++ 3819-rotate-non-negative-elements/3819-rotate-non-negative-elements.java
@@ -14,18 +14,39 @@ class Solution {
         int arr1[] = new int[countNonNeg];
         
         k = k%countNonNeg;
-        
+
+        // if(k == 0) return nums;
+
         for(int i=0; i<list.size(); i++){
             arr1[i] = nums[list.get(i)];
         }
+
+        int arr2[] = new int[k];
+        int arr3[] = new int[countNonNeg-k];
+
+        for(int i=0; i<k; i++){
+            arr2[i] = arr1[i];
+        }
+        for(int i=k,j=0; i<arr1.length; i++,j++){
+            arr3[j] = arr1[i];
+        }
+
+        for(int i=0; i<arr3.length; i++){
+            arr1[i] = arr3[i];
+        }
+
+        for(int i=arr3.length, j=0; j<arr2.length; i++,j++){
+            arr1[i] = arr2[j];
+        }
+
         // now do rotation countNonNeg times
        
         // reverse 1st k i.e., from 0 to k-1
-        reverse(0,k-1,arr1);
+        // reverse(0,k-1,arr1);
         // reverse elements from k to n-1
-        reverse(k,arr1.length-1,arr1);
+        // reverse(k,arr1.length-1,arr1);
         // reverse entire arr
-        reverse(0,arr1.length-1,arr1);
+        // reverse(0,arr1.length-1,arr1);
         // now reassign values in the original arr at appropriate indexes
         for(int i=0; i<list.size(); i++){
             nums[list.get(i)] = arr1[i];
@@ -34,13 +55,13 @@ class Solution {
         return nums;
     }
     // reverse
-    public static void reverse(int si, int ei, int arr[]){
-        while(si < ei){
-            int temp = arr[si];
-            arr[si] = arr[ei];
-            arr[ei] = temp;
-            si++;
-            ei--;
-        }
-    }
+    // public static void reverse(int si, int ei, int arr[]){
+    //     while(si < ei){
+    //         int temp = arr[si];
+    //         arr[si] = arr[ei];
+    //         arr[ei] = temp;
+    //         si++;
+    //         ei--;
+    //     }
+    // }
 }
