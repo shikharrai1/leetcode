6c3108d8aa83232ac06ca5e97e9839d20e1b28d4
Shikhar Rai <shikharrai398@gmail.com>
2026-02-25 09:39:17 +0530

    Time: 1 ms (24.48%), Space: 43 MB (41.41%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,16 +1,37 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        StringBuilder sb1 = new StringBuilder(s);
-        StringBuilder sb2 = new StringBuilder(goal);
 
-        if(sb1.compareTo(sb2) == 0) return true;
+        // Strings must be same length to be rotations of each other
+        // if (s.length() != goal.length()) {
+        //     return false;
+        // }
+        // StringBuilder sb1 = new StringBuilder(s);
+        // StringBuilder sb2 = new StringBuilder(goal);
 
-        for(int i=1; i<sb1.length(); i++){
-            char ch = sb1.charAt(0);
-            sb1.deleteCharAt(0);
-            sb1.append(ch);
-            if(sb1.compareTo(sb2) == 0) return true;
+        // if(sb1.compareTo(sb2) == 0) return true;
+
+        // for(int i=1; i<sb1.length(); i++){
+        //     char ch = sb1.charAt(0);
+        //     sb1.deleteCharAt(0);
+        //     sb1.append(ch);
+        //     if(sb1.compareTo(sb2) == 0) return true;
+        // }
+        // return false;
+
+                // Strings must be same length to be rotations of each other
+        if (s.length() != goal.length()) {
+            return false;
         }
+
+        // Try all possible rotations of s
+        for (int i = 0; i < s.length(); i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (rotated.equals(goal)) {
+                // Return true if a match is found
+                return true;  
+            }
+        }
+
         return false;
     }
 }
