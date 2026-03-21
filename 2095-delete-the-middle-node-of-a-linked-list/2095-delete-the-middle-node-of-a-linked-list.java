bed21569bad3466db2193ae330d748b9344aac1c
Shikhar Rai <shikharrai398@gmail.com>
2026-03-21 15:00:15 +0530

    Time: 3 ms (99.79%), Space: 202.4 MB (86.55%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -13,32 +13,50 @@ class Solution {
         // ----------------------App1----------------------
         // first find the length of the ll then divide that length by two to find the mid and then traverse upto the mid node
         
-        int len = getLen(head);
-        // check is size of LL is 1 so we will delete that 
-        if(len == 1) return head.next;
-        int mid = len/2;
-        // in order to delete the middle node we have to reach a node that is just before the middle node 
-        int nodeToReach = mid-1;
-        int cnt = 0;
-        ListNode prev = head;
-        while(cnt < nodeToReach){
-            prev = prev.next;
-            cnt++;
-        }
-        prev.next = prev.next.next;
-        return head;
-    }
+    //     int len = getLen(head);
+    //     // check is size of LL is 1 so we will delete that 
+    //     if(len == 1) return head.next;
+    //     int mid = len/2;
+    //     // in order to delete the middle node we have to reach a node that is just before the middle node 
+    //     int nodeToReach = mid-1;
+    //     int cnt = 0;
+    //     ListNode prev = head;
+    //     while(cnt < nodeToReach){
+    //         prev = prev.next;
+    //         cnt++;
+    //     }
+    //     prev.next = prev.next.next;
+    //     return head;
+    // }
 
-    // getLen()
-    int getLen(ListNode head){
-        // base case
-        if(head == null) return 0;
-        ListNode temp = head;
-        int len = 0;
-        while(temp != null){
-            len++;
-            temp = temp.next;
-        }
-        return len;
+    // // getLen()
+    // int getLen(ListNode head){
+    //     // base case
+    //     if(head == null) return 0;
+    //     ListNode temp = head;
+    //     int len = 0;
+    //     while(temp != null){
+    //         len++;
+    //         temp = temp.next;
+    //     }
+    //     return len;
+    // }
+
+    //-----------------Optimal App----------------------------
+    // using slow fast technique;
+    // base case
+    if(head.next == null) return head.next;
+    ListNode slow = head;
+    ListNode fast = head;
+    ListNode prev = head;
+    // find the mid node
+    while(fast != null && fast.next != null){
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
     }
+    // now when the slow is pointing at the mid node the prev pointer will be pointing to a node just before this mid node
+    prev.next = prev.next.next;
+    return head;
+}
 }
